5094d2e52e80aa364d9f18875ef523b79a41b4d3
Dr milli <100302736+drmilli@users.noreply.github.com>
2026-07-03 01:15:29 +0100

    done

---
+++ index.js
@@ -0,0 +1,5 @@
+// Entry point for Render deployment
+// This file is used by Render's default start command (node index.js)
+// It simply imports the actual server from server.js
+
+export { default } from "./server.js";
