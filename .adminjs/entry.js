e10bc127cd372b621ba63147a2be22c6e03f0c38
Dr milli <100302736+drmilli@users.noreply.github.com>
2026-09-12 03:46:51 +0100

    ad

--- .adminjs/entry.js
+++
@@ -1,7 +0,0 @@
-AdminJS.UserComponents = {}
-import Dashboard from '../admin/components/Dashboard'
-AdminJS.UserComponents.Dashboard = Dashboard
-import PushNotifications from '../admin/components/PushNotifications'
-AdminJS.UserComponents.PushNotifications = PushNotifications
-import Login from '../admin/components/Login'
-AdminJS.UserComponents.Login = Login
